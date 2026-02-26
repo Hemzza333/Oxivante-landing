a89fd1b5a6303ffc6d84fb66474026c411f45f37
Hamza <oussous901@gmail.com>
2026-02-26 08:07:41 +0000

    fix facebook pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,29 +54,35 @@ export default function RootLayout({
         />
       </head>
 
-      import Script from "next/script"
-
-<Script
-  strategy="afterInteractive"
-  dangerouslySetInnerHTML={{
-    __html: `
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '2556593138058294');
-      fbq('track', 'PageView');
-    `,
-  }}
-/>
-
       <body className={`${inter.variable} font-sans antialiased`}>
         {children}
         <Analytics />
+        import Script from "next/script";
+
+<Script
+  src="https://connect.facebook.net/en_US/fbevents.js"
+  strategy="afterInteractive"
+/>
+
+<Script
+  id="facebook-pixel"
+  strategy="afterInteractive"
+>
+{`
+  window.fbq = window.fbq || function() {
+    window.fbq.callMethod ?
+    window.fbq.callMethod.apply(window.fbq, arguments) :
+    window.fbq.queue.push(arguments)
+  };
+  if (!window._fbq) window._fbq = window.fbq;
+  window.fbq.push = window.fbq;
+  window.fbq.loaded = true;
+  window.fbq.version = '2.0';
+  window.fbq.queue = [];
+  fbq('init', '2556593138058294');
+  fbq('track', 'PageView');
+`}
+</Script>
       </body>
     </html>
   )
